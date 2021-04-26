8f14b51c95d7ff3c3606114ff62b51448894f0eb
LydiaStefanova-NOAA <48454097+LydiaStefanova-NOAA@users.noreply.github.com>
2021-04-25 20:18:35 -0400

    Update submit_prep_flx.sh

--- submit_prep_flx.sh
+++ submit_prep_flx.sh
@@ -14,5 +14,5 @@ ulimit -s unlimited
 set -x
 
 date
-bash benchmark_package.sh RUN_PREP=YES prep_flx=0 prep_flx=1 RUN_VERI=NO RUN_MJO=NO RUN_PLOT=NO 
+bash benchmark_package.sh RUN_PREP=YES prep_flx=0 prep_flx=1
 date
